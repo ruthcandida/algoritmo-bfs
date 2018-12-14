d030ea72923bb9915d0fc5749d1d72b7fabac27e
RuthCandida <ruth.sistem@gmail.com>
2018-12-14 08:39:03 -0200

    Update index.js

--- src/app/routes/index.js
+++ src/app/routes/index.js
@@ -2,24 +2,6 @@ const TreeController = require('../controllers/TreeController');
 const express = require('express');
 const router = express.Router();
 
-/**
- * @api {post} /bfs Processar árvore BFS
- * @apiDescription fetch all messages with past scheduling and send.
- * @apiVersion 1.0.0
- * @apiName Get by data and send message
- * @apiGroup BFS
- *
- * @apiHeader (header) {String} Athorization           Customer ID and customer secret. ex: client1:secret1 encoded for base64 is Y2xpZW50MTpzZWNyZXQx, then the value will be: 'Basic Y2xpZW50MTpzZWNyZXQx'.
- *
- * @apiSuccess {Object} result                         Processing result.
- * @apiSuccess {Number} result.schedules               Amount of processed schedules.
- * @apiSuccess {Number} result.retrievedFromQueue      Quantity of request retrieved from the fault queue.
- *
- * @apiError (Error) status    HTTP status code.
- * @apiError (Error) code      Vitta status code.
- * @apiError (Error) message   Friendly error description.
- * @apiError (Error) detail    Technical error description.
- */
 router.post('/bfs', (req, res, next) => {
     try {
         const nodesVisited = new TreeController().treeBfs(req.body);
